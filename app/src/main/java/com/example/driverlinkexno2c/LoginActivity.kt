e09442dfd4206011167bcfa4402e15a6a3f12e32
KetrolinSharon <ketrolinsharonkr95@gmail.com>
2026-02-03 15:16:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/driverlinkexno2c/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.driverlinkexno2c
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/driverlinkexno2c/SignupActivity.kt
@@ -0,0 +1,22 @@
+package com.example.driverlinkexno2c
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val signupBtn = findViewById<Button>(R.id.signupBtn)
+
+        signupBtn.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
